c1a6048bb5c62cd7d43c8e588b91bd6613a8c555
skiotto93 <fabio.forenza@hotmail.com>
2023-01-26 17:32:48 +0100

    Fix error

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -21,10 +21,10 @@ class ProjectSeeder extends Seeder
 
             $new_project = new Project();
     
-            $new_project->title = $faker->sentence();
+            $new_project->name = $faker->sentence();
             $new_project->description = $faker->text(100);
-            $new_project->name_client = $faker->words(2);
-            $new_project->slug = Str::slug($new_project->title, '-');
+            $new_project->name_client = $faker->sentence();
+            $new_project->slug = Str::slug($new_project->name, '-');
     
             $new_project->save();
         }
