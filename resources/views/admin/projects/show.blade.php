e75cf7a8ac1a53fb8b819cf0c0be18662b5a4298
skiotto93 <fabio.forenza@hotmail.com>
2023-01-29 16:18:48 +0100

    Fix show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,13 +6,20 @@
             <span>
                 <strong>Nome Progetto:</strong> {{$project->name}}
             </span>
-            <p>
+            <p class="my-3">
                 <strong>Descrizione: </strong>{{$project->description}}
             </p>
             <div>
-                <strong>Immagine: </strong><img src="{{asset('storage/' . $project->cover_image)}}" alt="{{$project->name}}">
+                <strong>Immagine: </strong>
             </div>
         </div>
-        <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Torna alla Dashboard</a>
+        <div>
+            @if ($project->cover_image)
+                <img class="my-4" src="{{asset('storage/' . $project->cover_image)}}" alt="{{$project->name}}">
+            @endif
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Torna alla Dashboard</a>
+        </div>
     </div>
 @endsection
